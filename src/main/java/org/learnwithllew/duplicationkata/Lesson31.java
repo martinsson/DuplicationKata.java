1ad4883bf3d8d8402862e04e392507e2c393f20a
johan martinsson <jm1974@hotmail.com>
2019-03-14 16:36:18 +0100

    done

--- src/main/java/org/learnwithllew/duplicationkata/Lesson31.java
+++ src/main/java/org/learnwithllew/duplicationkata/Lesson31.java
@@ -1,43 +1,85 @@
 package org.learnwithllew.duplicationkata;
 
-public class Lesson31 extends Song
-{
-  public void singSong(int style, String... names)
-  {
-    switch (style)
-    {
-      case 1 :
-        for (String name : names)
-        {
-          if (name.startsWith("L"))
-          {
-            sing("Hip Hip Horray! For " + name);
-          }
-          else
-          {
-            sing("Hello " + name + ", it's nice to meet you.");
-          }
-        }
-        break;
-      case 2 :
-        for (String name : names)
-        {
-          if (name.contains("a"))
-          {
-            sing(name.toUpperCase() + "! Yay " + name + "!");
-          }
-          else
-          {
-            sing("Hello " + name + ", it's nice to meet you.");
-          }
-        }
-        break;
-      case 3 :
-        for (String name : names)
-        {
-          sing("Hello " + name + ", it's nice to meet you.");
-        }
-        break;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lesson31 extends Song {
+    public void singSong(int style, String... names) {
+        Map<Integer, Singer> singerTypes = new HashMap<>() {{
+            put(1, new BirthDaySinger());
+            put(2, new YaySinger());
+            put(3, new BoringSinger());
+        }};
+        NoSinger defaultValue = new NoSinger();
+        Singer singer = singerTypes.getOrDefault(style, defaultValue);
+        singer.doSing(names);
     }
-  }
+
+
+    private boolean isRightPerson(String name) {
+        return name.contains("a");
+    }
+
+    interface Singer {
+
+
+        void doSing(String[] names);
+    }
+
+    private class BirthDaySinger extends BoringSinger {
+        public boolean isRightPerson(String name) {
+            return name.startsWith("L");
+        }
+
+        public String singSpecial(String name) {
+            return "Hip Hip Horray! For " + name;
+        }
+
+    }
+
+    private class YaySinger extends BoringSinger {
+
+        public boolean isRightPerson(String name) {
+            return name.contains("a");
+        }
+
+        @Override
+        public String singSpecial(String name) {
+            return name.toUpperCase() + "! Yay " + name + "!";
+        }
+
+    }
+
+    private class BoringSinger implements Singer {
+
+        public void doSing(String[] names) {
+            for (String name : names) {
+                if (this.isRightPerson(name)) {
+                    sing(this.singSpecial(name));
+
+                } else {
+                    sing(this.singBoring(name));
+                }
+            }
+        }
+
+        public boolean isRightPerson(String name) {
+            return false;
+        }
+
+        public String singSpecial(String name) {
+            return null;
+        }
+
+        public String singBoring(String name) {
+            return "Hello " + name + ", it's nice to meet you.";
+        }
+    }
+
+    private class NoSinger implements Singer {
+        @Override
+        public void doSing(String[] names) {
+
+        }
+    }
 }
