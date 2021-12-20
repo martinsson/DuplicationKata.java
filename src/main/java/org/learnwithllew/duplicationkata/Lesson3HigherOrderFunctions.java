2611c7f6c9f12240511ec39176e32af1d08c80b0
johan.martinsson <johan.martinsson@dexyos.fr>
2021-12-21 00:31:15 +0100

    solve Lesson3

--- src/main/java/org/learnwithllew/duplicationkata/Lesson3HigherOrderFunctions.java
+++ src/main/java/org/learnwithllew/duplicationkata/Lesson3HigherOrderFunctions.java
@@ -1,41 +1,37 @@
 package org.learnwithllew.duplicationkata;
 
-public class Lesson3HigherOrderFunctions extends Song
-{
-  public void singCheers()
-  {
-    int number = 2;
-    sing(number + "! ");
-    number = number + 2;
-    sing(number + "! ");
-    number = number + 2;
-    sing(number + "! ");
-    number = number + 2;
-    sing(number + "! ");
-    sing("Who do we appreciate?");
-    number = 17;
-    sing(number + "! ");
-    number = getNextPrime(number);
-    sing(number + "! ");
-    number = getNextPrime(number);
-    sing(number + "! ");
-    number = getNextPrime(number);
-    sing(number + "! ");
-    sing("These are the primes, that we find fine!");
-  }
-  private int getNextPrime(int number)
-  {
-    switch (number)
-    {
-      case 13 :
-        return 17;
-      case 17 :
-        return 19;
-      case 19 :
-        return 23;
-      case 23 :
-        return 29;
+import java.util.function.Function;
+
+public class Lesson3HigherOrderFunctions extends Song {
+    public void singCheers() {
+        repeat(0, this::addTwo, "Who do we appreciate?");
+
+        repeat(13, this::getNextPrime, "These are the primes, that we find fine!");
     }
-    return 0;
-  }
+
+    private void repeat(int number, Function<Integer, Integer> nextNumberFn, String closingPhrase) {
+        for (int i = 0; i < 4; i++) {
+            number = nextNumberFn.apply(number);
+            sing(number + "! ");
+        }
+        sing(closingPhrase);
+    }
+
+    private int addTwo(int number) {
+        return number + 2;
+    }
+
+    private int getNextPrime(int number) {
+        switch (number) {
+            case 13:
+                return 17;
+            case 17:
+                return 19;
+            case 19:
+                return 23;
+            case 23:
+                return 29;
+        }
+        return 0;
+    }
 }
